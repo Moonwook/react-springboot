eddb1ae7fb9103fa3318f6aaad448745cd30c655
hi94028 <hi94028@gmail.com>
2024-07-05 15:13:54 +0900

    chap09

--- src/components/member/LoginComponent.js
+++ src/components/member/LoginComponent.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 // import { loginPostAsync } from '../../slices/loginSlice';
 // import { useNavigate } from 'react-router-dom';
 import useCustomLogin from '../../hooks/useCustomLogin';
+import KakaoLoginComponent from './KakaoLoginComponent';
 
 const initState = {
   email: '',
@@ -98,6 +99,7 @@ const LoginComponent = () => {
           </div>
         </div>
       </div>
+      <KakaoLoginComponent />
     </div>
   );
 };
--- src/router/memberRouter.js
+++ src/router/memberRouter.js
@@ -3,6 +3,8 @@ import { Suspense, lazy } from 'react';
 const Loading = <div>Loading....</div>;
 const Login = lazy(() => import('../pages/member/LoginPage'));
 const Logout = lazy(() => import('../pages/member/LogoutPage'));
+const KakaoRedirect = lazy(() => import('../pages/member/KakaoRedirectPage'));
+const MemberModify = lazy(() => import('../pages/member/ModifyPage'));
 
 const memberRouter = () => {
   return [
@@ -19,6 +21,22 @@ const memberRouter = () => {
       element: (
         <Suspense fallback={Loading}>
           <Logout />
+        </Suspense>
+      ),
+    },
+    {
+      path: 'kakao',
+      element: (
+        <Suspense fallback={Loading}>
+          <KakaoRedirect />
+        </Suspense>
+      ),
+    },
+    {
+      path: 'modify',
+      element: (
+        <Suspense fallback={Loading}>
+          <MemberModify />
         </Suspense>
       ),
     },
